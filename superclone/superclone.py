eb3b2de718a89d2f10d9afc4d0ac2dd551a3a955
Rich Jones <rich@anomos.info>
2015-06-06 17:57:33 -0700

    first actually working version

--- superclone/superclone.py
+++ superclone/superclone.py
@@ -11,6 +11,7 @@ CVS = 'cvs'
 SUBVERSION = 'svn'
 
 HGWEB_IDENT = '<table class="bigtable">'
+GITWEB_IDENT = "<table summary=\'repository list\' class=\'list nowrap\'>"
 
 def superclone(args):
 
@@ -24,6 +25,9 @@ def superclone(args):
     if 'http:' in url:
         print "WARNING! You are about clone data over plaintext HTTP."
 
+    if url[-1] == '/':
+        url = url[:-1]
+        print url
     web_response = requests.get(url)
 
     if web_response.status_code != 200:
@@ -47,12 +51,14 @@ def extract_repos(html_content):
     if HGWEB_IDENT in html_content:
         scraper_type = MERCURIAL
         repos = extract_mercurial(html_content)
-
-    # elif git:
-    #   craper_type = GIT
-    # etc
-
+    # Git
+    elif GITWEB_IDENT in html_content:
+        scraper_type = GIT
+        repos = extract_git(html_content)
+    # SVN
+    # CVS
     else:
+        import pdb; pdb.set_trace()
         raise Exception("Content at given URL is not supported.")
 
 
@@ -73,13 +79,36 @@ def extract_mercurial(html_content):
 
     return repos
 
+def extract_git(html_content):
+
+    soup = BeautifulSoup(html_content)
+    table = soup.findAll('table')[2]
+
+    if not table:
+        raise Exception("Misclassification.")
+
+    repos = []
+    for row in table.findAll('tr')[1:]:
+        link = row.findAll('a')
+        if len(link) == 0:
+            continue
+        link = link[0]['href']
+        repos.append(link)
+
+    return repos
+
 def clone_repo(address, repo_type):
 
     if repo_type == MERCURIAL:
         command = MERCURIAL + ' clone ' + address
         print command
         sarge.run(command)
-
+    elif repo_type == GIT:
+        if 'gitweb' in address:
+            address = address.replace('gitweb', 'git')
+        command = GIT + ' clone ' + address
+        print command
+        sarge.run(command)
     else:
         raise Exception(repo_type, "is not yet supported.")        
 
